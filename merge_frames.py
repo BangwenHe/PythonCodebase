45a783944b7a0bb2382f1498bd9d4dc8f9ebb4e7
hebangwen <1925585604@qq.com>
2022-05-04 20:43:05 +0800

    feat: add command line args for merging frames

--- merge_frames.py
+++ merge_frames.py
@@ -1,19 +1,45 @@
+import argparse
+
 import cv2
 from pathlib import Path
 from tqdm import tqdm
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--frames_folder", type=str, required=True, help="frames folder")
+    parser.add_argument("--output_video_path", type=str, required=True, help="output video path")
+    parser.add_argument("--fps", type=int, default=30, help="video fps")
+    parser.add_argument("--width", type=int, default=-1, help="video width")
+    parser.add_argument("--height", type=int, default=-1, help="video height")
+    args = parser.parse_args()
+    return args
+
+
 def main():
-    frames_path = "exps/motr_r50_wanderlust/results/20150203_120911_968"
-    frames_path = Path(frames_path)
+    args = parse_args()
+    frames_folder = Path(args.frames_folder)
 
-    output_video_path = "exps/motr_r50_wanderlust/result.mp4"
+    output_video_path = args.output_video_path
     fourcc = cv2.VideoWriter_fourcc(*"MP4V")
-    video_writer = cv2.VideoWriter(output_video_path, fourcc, 30, (1152, 648), True)
+    fps = args.fps
+    width = args.width
+    height = args.height
 
-    img_list = sorted(frames_path.iterdir(), key=lambda x: int(x.name.split("_")[1].split(".")[0]))
+    image = cv2.imread(str(frames_folder / next(frames_folder.iterdir())))
+    if width == -1:
+        width = image.shape[1]
+    if height == -1:
+        height = image.shape[0]
+
+    video_writer = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height), True)
+
+    img_list = sorted(frames_folder.iterdir(), key=lambda x: int(x.name.split("_")[1].split(".")[0]))
     for img_path in tqdm(img_list):
         img = cv2.imread(str(img_path))
+        if img.shape[0] != height or img.shape[1] != width:
+            img = cv2.resize(img, (width, height))
+
         video_writer.write(img)
     
     video_writer.release()
